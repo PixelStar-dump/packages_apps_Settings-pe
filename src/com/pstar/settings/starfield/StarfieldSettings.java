90fcd82a7b42179e3ff475fc3cd5e6ba5ba5ebe7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-09 20:57:15 +0530

    Settings: Add statusbar clock preferences
    Co-authored-by: Lars Greiss <kufikugel@googlemail.com>
    Co-authored-by: Ali B <abittin@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I078403f2c705e42dbff7381ea410567261911a64
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- src/com/pstar/settings/starfield/StarfieldSettings.java
+++ src/com/pstar/settings/starfield/StarfieldSettings.java
@@ -20,6 +20,8 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.ContentResolver;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
@@ -38,6 +40,12 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
+import android.text.format.DateFormat;
+import android.view.Display;
+import android.view.DisplayCutout;
+import android.view.Surface;
+import android.view.View;
+
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -46,9 +54,17 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
+import com.pixelstar.support.preferences.SecureSettingListPreference;
+
 @SearchIndexable
 public class StarfieldSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
+
+    private static final String KEY_CLOCK_POSITION = "status_bar_clock_position";
+    private static final String KEY_CLOCK_AM_PM = "status_bar_am_pm";
+
+    private SecureSettingListPreference mClockPositionPref;
+    private SecureSettingListPreference mAmPmPref;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -56,6 +72,28 @@ public class StarfieldSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         addPreferencesFromResource(R.xml.starfield_settings);
         final Resources res = getResources();
+         mClockPositionPref = (SecureSettingListPreference) findPreference(KEY_CLOCK_POSITION);
+        mAmPmPref = (SecureSettingListPreference) findPreference(KEY_CLOCK_AM_PM);
+
+        boolean hasNotch = hasCenteredCutout(getActivity());
+        boolean isRtl = getResources().getConfiguration().getLayoutDirection()
+                == View.LAYOUT_DIRECTION_RTL;
+
+        // Adjust clock position pref for RTL and center notch
+        int entries = hasNotch ? R.array.status_bar_clock_position_entries_notch
+                : R.array.status_bar_clock_position_entries;
+        int values = hasNotch ? (isRtl ? R.array.status_bar_clock_position_values_notch_rtl
+                : R.array.status_bar_clock_position_values_notch)
+                : (isRtl ? R.array.status_bar_clock_position_values_rtl
+                : R.array.status_bar_clock_position_values);
+        mClockPositionPref.setEntries(entries);
+        mClockPositionPref.setEntryValues(values);
+
+        // Disable AM/PM for 24-hour format
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mAmPmPref.setEnabled(false);
+            mAmPmPref.setSummary(R.string.status_bar_am_pm_disabled);
+        }
     }
 
     @Override
@@ -79,6 +117,36 @@ public class StarfieldSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.CUSTOM;
     }
 
+    /* returns whether the device has a centered display cutout or not. */
+    private static boolean hasCenteredCutout(Context context) {
+        Display display = context.getDisplay();
+        DisplayCutout cutout = display.getCutout();
+        if (cutout != null) {
+            Point realSize = new Point();
+            display.getRealSize(realSize);
+
+            switch (display.getRotation()) {
+                case Surface.ROTATION_0: {
+                    Rect rect = cutout.getBoundingRectTop();
+                    return !(rect.left <= 0 || rect.right >= realSize.x);
+                }
+                case Surface.ROTATION_90: {
+                    Rect rect = cutout.getBoundingRectLeft();
+                    return !(rect.top <= 0 || rect.bottom >= realSize.y);
+                }
+                case Surface.ROTATION_180: {
+                    Rect rect = cutout.getBoundingRectBottom();
+                    return !(rect.left <= 0 || rect.right >= realSize.x);
+                }
+                case Surface.ROTATION_270: {
+                    Rect rect = cutout.getBoundingRectRight();
+                    return !(rect.top <= 0 || rect.bottom >= realSize.y);
+                }
+            }
+        }
+        return false;
+    }
+    
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
                 @Override
